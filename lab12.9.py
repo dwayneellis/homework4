46403d4724ede708f8b3c831a70f63c4af21f976
dwayneellis <70253282+dwayneellis@users.noreply.github.com>
2020-11-29 14:10:46 -0600

    Add files via upload

---
+++ lab12.9.py
@@ -0,0 +1,20 @@
+# CIS 2348 Homework 4 Fall 2020.
+# Dwayne Ellis
+# Student ID: 0833810
+# Lab 12.9 Exception handling to detect input string vs. integer
+
+
+if __name__ == "__main__":
+    # Split input into 2 parts: name and age
+    parts = input().split()
+    name = parts[0]
+    while name != '-1':
+        try:
+            age = int(parts[1]) + 1
+        except:
+            age = 0
+        print('{} {}'.format(name, age))
+
+        # Get next line
+        parts = input().split()
+        name = parts[0]
